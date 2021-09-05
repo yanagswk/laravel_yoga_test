aa72b4d42212f7eb58ef2b065e0c74bf98fb4491
yanagswk <yanagimassu@gmail.com>
2021-09-05 19:37:48 +0900

    残りの空き状況を返すテストメソッド実装 : remainingCount

--- app/app/Models/Lesson.php
+++ app/app/Models/Lesson.php
@@ -3,16 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Lesson extends Model
 {
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
     public function getVacancyLevelAttribute(): VacancyLevel
     {
         return new VacancyLevel($this->remainingCount());
     }
 
+    /**
+     * 残りの空き状況を返す
+     * $this->capacity 募集人数
+     * $this->reservations()->count() 予約件数
+     */
     private function remainingCount(): int
     {
-        return 0;
+        return $this->capacity - $this->reservations()->count();
     }
 }
--- app/tests/Feature/Http/Controllers/LessonControllerTest.php
+++ app/tests/Feature/Http/Controllers/LessonControllerTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 use App\Models\Lesson;
+use App\Models\Reservation;
+use App\Models\User;
 
 
 class LessonControllerTest extends TestCase
@@ -13,16 +15,58 @@ class LessonControllerTest extends TestCase
     // テスト実行時にデータベースを再構築
     use RefreshDatabase;
 
-    public function testShow()
-    {
+    /**
+     * @param int $capacity lessonモデルの募集人数
+     * @param int $reservationCount 予約回数
+     * @param string $expectedVacancyLevelMark 記号
+     * @dataProvider dataShow
+     */
+    public function testShow(
+        int $capacity,
+        int $reservationCount,
+        string $expectedVacancyLevelMark
+    ) {
         // テストデータ作成
-        $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン']);
+        $lesson = factory(Lesson::class)->create([
+            'name' => '楽しいヨガレッスン',
+            'capacity' => $capacity
+        ]);
+
+        // 予約回数分、userのfactoryを作る
+        for ($i = 0; $i < $reservationCount; $i++) {
+            $user = factory(User::class)->create();
+            factory(Reservation::class)->create([
+                'lesson_id' => $lesson->id,
+                'user_id' => $user->id
+            ]);
+        }
 
         // postリクエストの時はpost()を指定
         $response = $this->get("/lessons/{$lesson->id}");
 
         $response->assertStatus(Response::HTTP_OK);
         $response->assertSee($lesson->name);
-        $response->assertSee('空き状況: ×');
+        $response->assertSee("空き状況: {$expectedVacancyLevelMark}");
+    }
+
+    public function dataShow()
+    {
+        return [
+            '空き十分' => [
+                'capacity' => 6,
+                'reservationCount' => 1,
+                'expectedVacancyLevelMark' => '◎',
+            ],
+            '空きわずか' => [
+                'capacity' => 6,
+                'reservationCount' => 2,
+                'expectedVacancyLevelMark' => '△',
+            ],
+            '空きなし' => [
+                'capacity' => 1,
+                'reservationCount' => 1,
+                'expectedVacancyLevelMark' => '×',
+            ],
+        ];
     }
 }
